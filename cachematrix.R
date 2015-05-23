a0d7993e8eacf40ecfdfcbb366abd8351f043e92
sithumn <sithum.nissanka@ebuilder.com>
2015-05-23 16:58:45 +0530

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special matrix object which is
+## capable of caching the inverse of an invertible matrix.
+## This is specially useful since matrix inversion is a resource
+## consuming operation. Steps of the function are as follows;
+## 1. If a new matrix 'y' is received it will be assigned to x
+##     and reset cache
+## 2. Defines the get function to return the original matrix
+## 3. Defines the set function for the inverse matrix. This will
+##     cache the inverse of the matrix.
+## 4. Defines the get function for the inverse matrix
+##
+##
+## cacheSolve will return the inverse of a given
+## invertivle matrix. Steps as follows;
+## 1. Check if the inverse matrix is available in
+##     the cache. If found return the inverse matrix
+## 2. Else inverse will be calculated, cached and
+##     then will return.
 
-## Write a short comment describing this function
+
+## Creates a special matrix object which caches
+## the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInv <- function (inverse) i <<- inverse
+        getInv <- function () i
+        list(set = set, get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns an inverse of a given matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+        i <- x$getInv()
+        if (!is.null(i)) {
+                message("Getting cached data")
+                return (i)
+        }
+        data <- x$get()
+        i <- solve(data)
+        x$setInv(i)
+        i
 }
